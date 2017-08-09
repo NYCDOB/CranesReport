74c54c17be284c0a18e7b6b708670bd9ad32d430
bmancell@buildings.nyc.gov <BMancell@buildings.nyc.gov>
2017-08-09 16:49:20 -0400

    First commit

---
+++ js/topics.js
@@ -0,0 +1,51 @@
+var characteristics = { // eslint-disable-line
+  permits: {
+    displayName: 'Permits',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    colors: ['#1B83B0', '#C17229'],
+    about: 'Approved CN permits include the use of various equipment types, including derricks, fix/climber tower cranes, mast climbers, mobile cranes, mobile tower cranes, and pile drivers. There was a steady increase in the number of approved CN permits since 2012, with an 82% increase from 2012 to 2016 and a 23% increase from 2015 to 2016. In Manhattan, CD105 had the greatest number of CN permits approved in 2012 and 2013, while CD104 experienced the greatest number of permits approved in 2014 and 2015. In Brooklyn, CD302 experienced a spike of approved CN permits in 2014, with a 279% increase from the previous year.',
+    descriptor: 'people who reported as'
+  },
+  accident: {
+    displayName: 'Accidents',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CraneAccidents.html',
+    colors: ['#a6d1f8', '#012e60'],
+    about: 'Of all characteristics, age is one of the greatest predictors of migration. NYC consistently attracts large numbers of people in their 20s, and generally sees net migration losses of people in all other age groups. This is tied to the lifecycle of migration, whereby young single people move to the city, and leave after family formation. Over the past 30 years, net migration among those in their 20s has grown more positive.  While net migration was still negative for most other age groups, the most recent period has seen a reduction in net losses for most age groups.',
+    descriptor: 'people aged'
+  },
+  cn: {
+    displayName: 'On-Site Certificate',
+	chartMap: 'https://nycdob.github.io/ActiveB_AvgAge_Map/',
+    colors: ['#a6d1f8', '#012e60'],
+    about: 'Of all characteristics, age is one of the greatest predictors of migration. NYC consistently attracts large numbers of people in their 20s, and generally sees net migration losses of people in all other age groups. This is tied to the lifecycle of migration, whereby young single people move to the city, and leave after family formation. Over the past 30 years, net migration among those in their 20s has grown more positive.  While net migration was still negative for most other age groups, the most recent period has seen a reduction in net losses for most age groups.',
+    descriptor: 'people aged'
+  },
+  cd: {
+    displayName: 'Certificate of Operation',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    colors: ['#1B83B0', '#C17229'],
+    about: 'Historically, males and females are generally equally likely to move in or out of the city. However, in the 2010-2014 period, NYC experienced a net gain of 44,000 more women than men due to migration.',
+    descriptor: 'people who reported as'
+  },
+  type: {
+    displayName: 'Equipment Type',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    colors: ['#1f4064', '#fdc010', '#cd594a', '#73c4e1', '#5e170e'],
+    about: 'Each race/Hispanic group has shown a different pattern over the last 30 years. The City’s fiscal crisis in the 1970s saw a big loss of the white population, with smaller losses in the decades that followed. 2010-2014 shows positive net migration for white New Yorkers. The black population has shown consistent net migration losses since the 70s, a reversal of the trend in the earlier part of the 20th century that saw net migration for the black population surge, especially from the southern states.  Hispanics, too, have experienced net migration losses since the 70s, but their losses have been paired back and were close to zero in the latest time period.  Asians are the only major race/ethnic group to have positive net migration since 1975.',
+    descriptor: 'people who reported their race as'
+  },
+	age: {
+	displayName: 'Equipment Age',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+	colors: ['#a6d1f8', '#012e60'],
+	about: 'Of all characteristics, age is one of the greatest predictors of migration. NYC consistently attracts large numbers of people in their 20s, and generally sees net migration losses of people in all other age groups. This is tied to the lifecycle of migration, whereby young single people move to the city, and leave after family formation. Over the past 30 years, net migration among those in their 20s has grown more positive.  While net migration was still negative for most other age groups, the most recent period has seen a reduction in net losses for most age groups.',
+	descriptor: 'people aged'
+  },
+  ahv: {
+    displayName: 'After Hours Variances',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    colors: ['#1B83B0', '#C17229'],
+    about: 'Historically, males and females are generally equally likely to move in or out of the city. However, in the 2010-2014 period, NYC experienced a net gain of 44,000 more women than men due to migration.',
+    descriptor: 'people who reported as'
+  }
+};
